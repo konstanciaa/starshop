a7538d35d370998a50d05ad1616281c7d1167519
Anna Konstantynova <konstanna.dev@gmail.com>
2024-06-17 09:34:02 +0200

    Install twig pack

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,10 +5,9 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController 
+class MainController
 {
     #[Route('/')]
-
     public function homepage(): Response
     {
         return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
